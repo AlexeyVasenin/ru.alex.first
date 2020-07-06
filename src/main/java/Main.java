edbca9b56e3d3c824070884cd4ca6544c0b313b2
kusakin-fs <kusakin-fs@ural.tr.ru>
2020-07-06 21:42:55 +0500

    Lesson 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi fil  !!");
+        System.out.println(multiply(2, 3));
+    }
+
+    private static Float multiply(float x, float y) {
+        return x * y;
     }
 }
