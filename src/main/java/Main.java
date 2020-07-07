5ae1521d4560c58f209a3754690a6107408f1bd7
Alexey <alexxx1809@gmail.com>
2020-07-07 15:08:53 +0500

    Начал выполнять первую задачу

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi fil  !!");
-        System.out.println(multiply(2, 3));
+
+        //Сложение сторок
+        String a = "Hello ";
+        String b = "World!";
+        System.out.println(a + b);
+
+        StringBuilder c = new StringBuilder(a);
+//        System.out.println(c.append("df"));
+        System.out.println(c.insert(6, "World!"));
+
+
+        //Результаты методов
+        System.out.println(multiply(3.1f, 3.1f));
+        System.out.println(multiply2(3.1, 3.1));
+
+        //Сравениен результатов
+
     }
 
+    // Метод Float
     private static Float multiply(float x, float y) {
         return x * y;
     }
+
+    //Метод Double
+    private static Double multiply2(double x, double y) {
+        return x * y;
+    }
+
+
 }
