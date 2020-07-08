ddac2386205a07894a77a4be871b5db40e4dccde
Alexey <alexxx1809@gmail.com>
2020-07-08 14:10:28 +0500

    Добавил метод Boolean и сравнил результаты методов multiply и multiply2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,27 +6,34 @@ public class Main {
         String b = "World!";
         System.out.println(a + b);
 
+//      Слодение строк через StringBuilder
         StringBuilder c = new StringBuilder(a);
-//        System.out.println(c.append("df"));
+//      System.out.println(c.append("World!"));
         System.out.println(c.insert(6, "World!"));
-
 
         //Результаты методов
         System.out.println(multiply(3.1f, 3.1f));
         System.out.println(multiply2(3.1, 3.1));
+        System.out.println(result());
 
-        //Сравениен результатов
+        //Сравениен результатов?!
 
     }
 
-    // Метод Float
+    //Метод Float
     private static Float multiply(float x, float y) {
         return x * y;
+
     }
 
     //Метод Double
     private static Double multiply2(double x, double y) {
         return x * y;
+    }
+
+    //Метод сравнение
+    private static Boolean result() {
+        return multiply(2.1f, 2.1f) >= multiply2(2.1, 2.1);
     }
 
 
