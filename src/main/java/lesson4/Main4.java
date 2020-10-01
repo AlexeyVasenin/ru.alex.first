d24b256477a88bb635d306ceb4457adae83b2684
Alexey <alexxx1809@gmail.com>
2020-10-01 09:21:08 +0500

    Исправление замечаний...

--- src/main/java/lesson4/Main4.java
+++ src/main/java/lesson4/Main4.java
@@ -6,14 +6,17 @@ import java.io.InputStreamReader;
 
 
 public class Main4 {
+
     private static final BufferedReader in =
             new BufferedReader(new InputStreamReader(System.in));
     private static int n;
-    private static float value;
+    private static double x;
 
-    //TODO: У тебя функция бесконечно убывающая от 1 до 0, как точность может быть >=1?
-    private static void inputNumberToPower() {
-        System.out.println("Введите степень точности от 1 до ∞");
+    //TODO: У тебя функция бесконечно убывающая от 1 до 0, как точность может
+    // быть >=1?
+    private static void inputNumberToPower()
+    {
+        System.out.println("Введите число членов разложения от 2 до ∞");
         while (true) {
             try {
                 n = Integer.parseInt(in.readLine());
@@ -21,69 +24,71 @@ public class Main4 {
             } catch (Exception e) {
                 e.printStackTrace();
                 System.out.println("Вы ввели не число или число дробное, " +
-                        "введите повторно число от 1 до ∞");
+                        "введите повторно число от 2 до ∞");
             }
         }
+
     }
 
     private static void inputNumberToValue()
     {
-        System.out.println("Введите число меньше 1");
+        System.out.println("Введите значение x больше -1 до 1 включительно");
         while (true) {
             try {
-                value = Float.parseFloat(in.readLine());
+                x = Double.parseDouble(in.readLine());
                 //TODO: какое условие сходимости ряда?
-                if (value >= 1) {
-                    //TODO: сделай выозов ошибки. (throw new Exception()) с описанием ошибки и выведи это описание
-                    System.out.println("Вы введи число больше или равное 1 " +
-                            "повторите " +
-                            "ввод число меньше 1");
-                    value = Float.parseFloat(in.readLine());
+                if (x > 1 || x <= -1) {
+                    throw new Exception("Вы введи число больше 1 или меньше " +
+                            "и равное -1 повторите ввод"); // TODO: 30.0.2020
+                    // Сделал вызов ошибки
+                    //TODO: сделай выозов ошибки. (throw new Exception()) с
+                    // описанием ошибки и выведи это описание
                 } else {
                     break;
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-                System.out.println("Вы ввели не число" +
-                        "введите повторно число от 1 до ∞");
+                System.out.println("Повторите ввод число больше -1 или меньше" +
+                        "и равное 1");
             }
         }
     }
 
     //TODO: n-константа и n-как параметр метода?
     //TODO: Не понятно по имени метода за что отвечает метод
-    private static float func(int n) throws IOException {
-        float x = value;
-        if (n == 1) {
-            return (-x);
-        }
 
+    // TODO: 30.09.2020 Изменил имя метода
+    private static double ResultOfCalcTheFuncLn(int n) throws IOException
+    {
+        if (n == 1) {
+            return (x);
+        }
         //TODO: Почему нельзя присвоить значение сразу переменной?
-        float result;
-        result = (float) (func(n - 1) - ((Math.pow(-1, n) * Math.pow(-x, n)) / n));
         //TODO: Почему нельзя сразу вернуть result?
-        return result;
+        return (double) (ResultOfCalcTheFuncLn(n - 1) - ((Math.pow(-1, n) * Math.pow(x, n)) / n)); // TODO: 30.09.2020 Сократил код
     }
 
     //TODO: n-константа и n-как параметр метода?
     //TODO: Не понятно по имени метода за что отвечает метод
-    private static float func1(int n) {
-        float x = value;
-        float result = 0;
+    private static double func1()
+    {
+        double result = 0;
 
         for (int i = 1; i <= n; i++) {
-            result -= (float) ((Math.pow(-1, i) * Math.pow(-x, i)) / i);
+            result -= (float) ((Math.pow(-1, i) * Math.pow(x, i)) / i);
         }
 
-        return (result);
+        return result;
     }
 
     //TODO: Не верное решение
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("Программа для вычисления функции Ln(1+x)=... \n");
         inputNumberToPower();
         inputNumberToValue();
         //TODO: Зачем n используешь как константу?
-        System.out.println("Result recursive func \n" + func(n));
-        System.out.println("\n Result cycle func1 \n" + func1(n));
+        System.out.println("Result recursive func Ln(1+x) \n" + ResultOfCalcTheFuncLn(n));
+        System.out.println("\nResult cycle func Ln(1+x) \n" + func1());
     }
 }
